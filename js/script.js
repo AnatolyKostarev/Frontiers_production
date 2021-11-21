483090024c6a57be78b271326a91b2fa9f94bcd3
Anatoly <anatolykostarev@gmail.com>
2021-11-21 23:11:53 +0300

    Add comments for data keeping in localStorage

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const investorLink = document.querySelector(".js-investor");
 const entrepreneurLink = document.querySelector(".js-entrepreneur");
 const entrepreneurList = document.querySelector(".js-entrepreneurServiceList");
 const investorList = document.querySelector(".js-investorServiceList");
+const searchForm = document.querySelector(".js-news-form");
+const searchInput = document.querySelector(".js-news-search");
 
 const showInvestor = (e) => {
   e.preventDefault();
@@ -30,3 +32,23 @@ const shownEtrepreneur = (e) => {
 
 investorLink.addEventListener("click", showInvestor);
 entrepreneurLink.addEventListener("click", shownEtrepreneur);
+
+// создаем массив
+// let searchArr = [];
+
+// подписываемся на форму
+// searchForm.addEventListener("submit", (e) => {
+// предотвращаем отправку формы
+// e.preventDefault();
+// добавляем в массив значения введенные пользователем
+// searchArr.push(searchInput.value);
+// очищаем поле input после ввода значения
+// searchInput.value = "";
+// передаем в localStorage массив в формате JSON, так как данные хранятся в виде строки
+// localStorage.setItem("userData", JSON.stringify(searchArr));
+// в константу кладем массив, полученный из localStorage в формате строки и распарсенный в массив
+// const data = JSON.parse(localStorage.getItem("userData"));
+
+//далее используем массив
+// console.log(data);
+// });
